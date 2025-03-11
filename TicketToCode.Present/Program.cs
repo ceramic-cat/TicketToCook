520f7dbad452df8cdb5a9019aa64f9f065c9f44b
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2025-03-11 17:54:32 +0100

    Added blazor and first attempt at login-view. Implementation not yet done.#

--- TicketToCode.Present/Program.cs
+++ TicketToCode.Present/Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddScoped(sp =>
+    new HttpClient { BaseAddress = new Uri("https://localhost:5235") });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
