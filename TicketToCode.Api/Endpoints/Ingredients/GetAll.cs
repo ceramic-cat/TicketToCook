6ec9e314ad79bc6a263ddb770ec36a433159a937
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2025-03-12 13:55:35 +0100

    Created endpoints for Ingredients. Update is having some issues, but thats a later problem. Removed Event collections in the Database

---
+++ TicketToCode.Api/Endpoints/Ingredients/GetAll.cs
@@ -0,0 +1,27 @@
+﻿namespace TicketToCode.Api.Endpoints.Ingredients;
+
+public class GetAll : IEndpoint
+{
+    public static void MapEndpoint(IEndpointRouteBuilder app) => app
+    .MapGet("/Ingredients", Handle)
+    .WithSummary("Get all Ingredients");
+
+    // DTO's
+
+    public record Response(int Id, string Name, IngredientType Type);
+
+    // Logic
+    private static List<Response> Handle(IDatabase db)
+    {
+        return db.Ingredients
+            .Select(item => new Response(
+                item.Id,
+                item.Name,
+                item.Type))
+            .ToList();
+
+    }
+
+
+    }
+
