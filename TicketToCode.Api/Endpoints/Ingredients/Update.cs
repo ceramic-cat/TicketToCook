c4b2c081aa60f53e721972b1f14509bec92f962b
Whateverness <nicolefritz00@gmail.com>
2025-03-14 10:02:38 +0100

    Fixed Update Endpoints

--- TicketToCode.Api/Endpoints/Ingredients/Update.cs
+++ TicketToCode.Api/Endpoints/Ingredients/Update.cs
@@ -2,26 +2,26 @@
 
 namespace TicketToCode.Api.Endpoints.Ingredients;
 
-public class UpdateIngredient
-    // MapEndpoints doesn't seem to find this for some reason.
+public class UpdateIngredient : IEndpoint
 {
     public static void MapEndpoint(IEndpointRouteBuilder app) => app
-    .MapPut("/Ingredients/{id}", Handle)
-    .WithTags("Ingredients")
-    .WithSummary("Update an Ingredient");
+        .MapPatch("/Ingredients/Update/{id}", Handle)
+        .WithTags("Ingredients")
+        .WithSummary("Update an Ingredient");
 
-    // DTO's
+    // DTOs
     public record Request(
         int Id,
         string Name,
         IngredientType Type);
+
     public record Response(
         int Id, 
         string Name, 
         IngredientType Type);
 
     // Logic
-    private static Results<Ok<Response>, BadRequest<string>> Handle([AsParameters]Request request, IDatabase db)
+    private static Results<Ok<Response>, BadRequest<string>> Handle([AsParameters] Request request, IDatabase db)
     {
         var ingredient = db.Ingredients.Where(x => x.Id == request.Id).FirstOrDefault();
         if (ingredient is null)
@@ -29,14 +29,11 @@ public class UpdateIngredient
             return TypedResults.BadRequest("There's no ingredient with that Id");
         }
 
-        // patch the item
+        // Patch the item
         ingredient.Name = request.Name;
         ingredient.Type = request.Type;
 
         var response = new Response(ingredient.Id, ingredient.Name, ingredient.Type);
         return TypedResults.Ok(response);
-
     }
-
-
 }
